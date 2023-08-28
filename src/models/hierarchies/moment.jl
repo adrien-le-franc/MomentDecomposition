5a25b313d18831141946572b43f733bcad2dd32d
adrien-le-franc <adrien.le-franc@eleves.enpc.fr>
2023-08-28 20:03:34 +0200

    tssos moment

--- src/models/hierarchies/moment.jl
+++ src/models/hierarchies/moment.jl
@@ -3,38 +3,53 @@
 # generic sparse model for the hierarchy of moment relaxations
 
 
-function set_moment_matrices!(model::Model, pop::POP, relaxation_order::Int64, 
-	sets::Vector{Vector{T}}) where T <: Integer
+function set_moment_matrices!(model::Model, variable_sets::Vector{Vector{T}}, monomial_sets, relaxation_order::Int64) where T <: Integer
 
 	moment_labels = Dict{Vector{UInt16}, Int64}() 
-	n_monomials = n_moments(pop, relaxation_order)
 	y = Vector{VariableRef}()
 	count = 0
 
-	for set in sets
+	for (k, set) in enumerate(variable_sets)
 
-		n_monomials = n_moments(set, relaxation_order)
-		M = Matrix{AffExpr}(undef, n_monomials, n_monomials)
-		
-		for (j, alpha) in moment_columns(set, relaxation_order)
-			for (i, beta) in moment_rows(set, relaxation_order, j)
+		blocks = compute_tsp_blocks_X_0(set, monomial_sets[k], relaxation_order)
 
-				label = monomial_product(alpha, beta)
+		for (l, block) in matrix_blocks(blocks)
 
-				if !(label in keys(moment_labels))
+			M = Matrix{AffExpr}(undef, length(block), length(block))	
 
-					count += 1
-					push!(y, @variable(model, base_name="y_$(count)")) # base_name can be costly for large models: remove ?
-					moment_labels[label] = count
-				
+			for (j, (_, alpha)) in moment_columns(block, set, relaxation_order)
+				for (i, (_, beta)) in moment_rows(block, set, relaxation_order, j)
+
+					monomial = monomial_product(alpha, beta)
+
+					if !(monomial in keys(moment_labels))
+						count += 1
+						moment_labels[monomial] = count
+						push!(y, @variable(model)) 
+					end
+
+					M[i, j] = y[moment_labels[monomial]]					
+
 				end
-
-				M[i, j] = y[moment_labels[label]]
-
 			end
+
+			@constraint(model, Symmetric(M) >= 0, PSDCone())
+
 		end
 
-		@constraint(model, Symmetric(M) >= 0, PSDCone())
+		for (l, (_, alpha)) in moment_columns(diagonal_block(blocks), set, relaxation_order)
+
+			monomial = monomial_product(alpha, alpha)
+
+			if !(monomial in keys(moment_labels))
+				count += 1
+				moment_labels[monomial] = count
+				push!(y, @variable(model)) 
+			end
+
+			@constraint(model, y[moment_labels[monomial]] >= 0.)
+
+		end
 
 	end
 
@@ -63,28 +78,61 @@ end
 function set_inequality_constraint!(model::Model, pop::POP, polynomial::SparsePolynomial, 
 	relaxation_order::Int64, moment_labels::Dict{Vector{UInt16}, Int64}, 
 	variable_sets::Vector{Vector{T}}) where T <: Integer
-
+	
 	localizing_order = localizing_matrix_order(polynomial, relaxation_order)
+	count = length(moment_labels)
 
 	if localizing_order == 0
 
-		@constraint(model, linear_expression(model, polynomial, moment_labels) >= 0)
+		expression = AffExpr(0.)
+
+		for (support, coefficient) in terms(polynomial)
+
+			if !(support in keys(moment_labels))
+				count += 1
+				moment_labels[support] = count
+				push!(model[:y], @variable(model))
+			end
+
+			add_to_expression!(expression, coefficient, model[:y][moment_labels[support]])
+
+		end
+
+		@constraint(model, expression >= 0)
 
 	else
 
-		k = assign_constraint_to_set(polynomial, variable_sets)
-		n_monomials = n_moments(variable_sets[k], localizing_order)
+		#k = assign_constraint_to_set(polynomial, variable_sets)
+		#set = unique(vcat(polynomial.support...))
+		set = unique(vcat(polynomial.support...))
+
+		n_monomials = n_moments(set, localizing_order)
 		M = Matrix{AffExpr}(undef, n_monomials, n_monomials)
 
-		for (j, alpha) in moment_columns(variable_sets[k], localizing_order)
-			for (i, beta) in moment_rows(variable_sets[k], localizing_order, j)
+		for (j, alpha) in moment_columns(set, localizing_order)
+			for (i, beta) in moment_rows(set, localizing_order, j)
 
-				labels = [monomial_product(alpha, beta, gamma) for gamma in polynomial.support]
-				M[i, j] = linear_expression(model, polynomial, labels, moment_labels)
+				expression = AffExpr(0.)
+
+				for (gamma, coefficient) in terms(polynomial)
+
+					monomial = monomial_product(alpha, beta, gamma)
+
+					if !(monomial in keys(moment_labels))
+						count += 1
+						moment_labels[monomial] = count
+						push!(model[:y], @variable(model))
+					end
+
+					add_to_expression!(expression, coefficient, model[:y][moment_labels[monomial]])
+
+				end
+
+				M[i, j] = expression
 
 			end
 		end
-
+		
 		@constraint(model, Symmetric(M) >= 0, PSDCone())
 
 	end
@@ -98,21 +146,52 @@ function set_equality_constraint!(model::Model, pop::POP, polynomial::SparsePoly
 	variable_sets::Vector{Vector{T}}) where T <: Integer
 	
 	localizing_order = localizing_matrix_order(polynomial, relaxation_order)
+	count = length(moment_labels)
 
 	if localizing_order == 0
 
-		@constraint(model, linear_expression(model, polynomial, moment_labels) == 0)
+		expression = AffExpr(0.)
+
+		for (support, coefficient) in terms(polynomial)
+
+			if !(support in keys(moment_labels))
+				count += 1
+				moment_labels[support] = count
+				push!(model[:y], @variable(model))
+			end
+
+			add_to_expression!(expression, coefficient, model[:y][moment_labels[support]])
+
+		end
+
+		@constraint(model, expression == 0)
 
 	else
 
-		k = assign_constraint_to_set(polynomial, variable_sets)
+		#k = assign_constraint_to_set(polynomial, variable_sets)
+		#set = unique(vcat(polynomial.support...))
+		set = unique(vcat(polynomial.support...))
 
-		for (j, alpha) in moment_columns(variable_sets[k], localizing_order)
-			for (i, beta) in moment_rows(variable_sets[k], localizing_order, j)
+		for (j, alpha) in moment_columns(set, localizing_order)
+			for (i, beta) in moment_rows(set, localizing_order, j)
 
-				labels = [monomial_product(alpha, beta, gamma) for gamma in polynomial.support]
-				@constraint(model, linear_expression(model, polynomial, 
-					labels, moment_labels) == 0.)
+				expression = AffExpr(0.)
+
+				for (gamma, coefficient) in terms(polynomial)
+
+					monomial = monomial_product(alpha, beta, gamma)
+
+					if !(monomial in keys(moment_labels))
+						count += 1
+						moment_labels[monomial] = count
+						push!(model[:y], @variable(model))
+					end
+
+					add_to_expression!(expression, coefficient, model[:y][moment_labels[monomial]])
+
+				end
+
+				@constraint(model, expression == 0)
 
 			end
 		end			
@@ -161,14 +240,19 @@ function set_probability_measure_constraint!(model::Model,
 end
 
 function moment_relaxation_model(pop::POP, relaxation_order::Int64, 
-	sparsity_sets::Vector{Vector{T}}=dense_relaxation_set(pop)) where T <: Integer
+	variable_sets::Vector{Vector{T}},  
+	# =dense_relaxation_set(pop),
+	monomial_sets::Vector{Vector{Vector{UInt16}}};
+	minimal_multipliers=true,
+	moment_scaling=false,
+	return_monomials=false) where T <: Integer
 
 	model = Model()
 
-	moment_labels = set_moment_matrices!(model, pop, relaxation_order, sparsity_sets)
+	moment_labels = set_moment_matrices!(model, variable_sets, monomial_sets, relaxation_order)
 	set_objective!(model, pop, moment_labels)
 	set_probability_measure_constraint!(model, moment_labels)
-	set_polynomial_constraints!(model, pop, relaxation_order, moment_labels, sparsity_sets)
+	set_polynomial_constraints!(model, pop, relaxation_order, moment_labels, variable_sets)
 
 	return model
 
